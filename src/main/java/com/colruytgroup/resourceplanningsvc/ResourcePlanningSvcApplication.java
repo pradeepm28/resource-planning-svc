8fb2c61a2b10087f6678d81af12c059a226038cf
Pradeep Mettupalli <pradeepkumarreddy.mettupalli@colruytgroup.com>
2024-04-25 07:31:17 +0200

    Initial Version

---
+++ src/main/java/com/colruytgroup/resourceplanningsvc/ResourcePlanningSvcApplication.java
@@ -0,0 +1,13 @@
+package com.colruytgroup.resourceplanningsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcePlanningSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourcePlanningSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colruytgroup/resourceplanningsvc/ResourcePlanningSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colruytgroup.resourceplanningsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcePlanningSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
